36e7fab16d5060ba13950b673d2ddafa626ef02d
yasinabdmahmood <abd181103@gmail.com>
2023-01-12 00:12:56 +0300

    Fix linter errors

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Greeting from './Greeting';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 // import "./controllers"
-import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App'
 import { Provider } from 'react-redux';
+import App from './components/App';
 import store from './redux/store';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
